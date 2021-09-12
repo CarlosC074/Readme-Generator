c316545156212c5a045d0d85924716e401e89214
CarlosC074 <carlos.castano74@gmail.com>
2021-09-11 21:32:53 -0700

    the generateMarkdown module is mostly produced. TODO: finish the moule, add the inquirer questions, and the response functions

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 legalMaker = require('./utils/generateMarkdown.js')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What is the name of your project?", "What is your name/Github username?", "Provide a description of your project", "What are the steps to installation?"];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,3 +11,4 @@ function init() {}
 
 // Function call to initialize app
 init();
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,26 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  const userLicense = license;
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+  if(userLicense !== null){
+    return `[${data.projectname}](https://img.shields.io/badge/license-${userLicense}-brightgreen)`;
+  }
+  else {return ' '}
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  const userLicense = license;
+  
+  if (userLicense !== null) {
+    return `## license \n \n
+    ${userLicense} License \n \n
+    © ${data.year} ${data.name} \n \n
+    `
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -17,4 +29,4 @@ function generateMarkdown(data) {
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports = {renderLicenseBadge, renderLicenseSection, generateMarkdown};
